9f9d6952396633a65594abfe5175dfc903543273
Sebastian Mößler <s.moeszler@binsoul.de>
2019-11-26 19:12:22 +0100

    Extend IntlAddressFormatter instead of DefaultAddressFormatter

--- src/Formatter/AddressFormatter.php
+++ src/Formatter/AddressFormatter.php
@@ -4,27 +4,8 @@ declare(strict_types=1);
 
 namespace BinSoul\Symfony\Bundle\I18n\Formatter;
 
-use BinSoul\Common\I18n\DefaultAddressFormatter;
-use BinSoul\Common\I18n\DefaultLocale;
-use BinSoul\Common\I18n\Locale;
-use Symfony\Component\Intl\Countries;
-use Symfony\Component\Intl\Exception\MissingResourceException;
+use BinSoul\Common\I18n\Intl\IntlAddressFormatter;
 
-class AddressFormatter extends DefaultAddressFormatter
+class AddressFormatter extends IntlAddressFormatter
 {
-    public function __construct(?Locale $locale = null)
-    {
-        parent::__construct($locale ?? DefaultLocale::fromString(\Locale::getDefault()));
-    }
-
-    protected function isoCodeToName(string $isoCode): string
-    {
-        try {
-            $result = Countries::getName($isoCode, $this->locale->getCode('_'));
-        } catch (MissingResourceException $e) {
-            $result = $isoCode;
-        }
-
-        return $result;
-    }
 }
