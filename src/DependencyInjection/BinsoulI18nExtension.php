0943c7a9910cfecc2419b03b37a040c234efdad0
Sebastian Mößler <s.moeszler@binsoul.de>
2019-12-04 08:07:51 +0100

    Clear tags if prefix is empty

--- src/DependencyInjection/BinsoulI18nExtension.php
+++ src/DependencyInjection/BinsoulI18nExtension.php
@@ -19,8 +19,12 @@ class BinsoulI18nExtension extends Extension
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        $prefix = trim($config['prefix'] ?? '');
 
         $definition = $container->getDefinition(TablePrefixListener::class);
-        $definition->setArgument(0, $config['prefix']);
+        $definition->setArgument(0, $prefix);
+        if ($prefix === '') {
+            $definition->clearTags();
+        }
     }
 }
