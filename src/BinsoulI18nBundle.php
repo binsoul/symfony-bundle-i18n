3f0a4d0fa7673881670d51e521e1eae777e79773
Sebastian Mößler <s.moeszler@binsoul.de>
2024-10-15 16:59:08 +0200

    Don't use provided method to create DoctrineOrmMappingsPass

--- src/BinsoulI18nBundle.php
+++ src/BinsoulI18nBundle.php
@@ -6,8 +6,10 @@ namespace BinSoul\Symfony\Bundle\I18n;
 
 use BinSoul\Symfony\Bundle\I18n\DependencyInjection\Compiler\OverrideTranslatorPass;
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BinsoulI18nBundle extends Bundle
@@ -18,15 +20,18 @@ class BinsoulI18nBundle extends Bundle
 
         $container->addCompilerPass(new OverrideTranslatorPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 16);
 
-        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+        $container->addCompilerPass(
+            $this->buildDoctrineOrmMappingsPass(
+                ['BinSoul\Symfony\Bundle\I18n'],
+                [realpath(__DIR__ . '/Entity')],
+            )
+        );
+    }
 
-        if (class_exists($ormCompilerClass)) {
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createAttributeMappingDriver(
-                    ['BinSoul\Symfony\Bundle\I18n'],
-                    [realpath(__DIR__ . '/Entity')],
-                )
-            );
-        }
+    private function buildDoctrineOrmMappingsPass(array $namespaces, array $directories): DoctrineOrmMappingsPass
+    {
+        $driver = new Definition(AttributeDriver::class, [$directories]);
+
+        return new DoctrineOrmMappingsPass($driver, $namespaces, [], false, []);
     }
 }
